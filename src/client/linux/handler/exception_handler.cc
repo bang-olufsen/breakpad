5963db73d7e30f693efaa7b2210d8be9f79da085
Jeppe Frandsen <jeppe@algrens.dk>
2018-03-02 11:35:10 +0100

    Added SIGXCPU to exception signals

--- src/client/linux/handler/exception_handler.cc
+++ src/client/linux/handler/exception_handler.cc
@@ -112,7 +112,7 @@ namespace {
 // all these signals must be Core (see man 7 signal) because we rethrow the
 // signal after handling it and expect that it'll be fatal.
 const int kExceptionSignals[] = {
-  SIGSEGV, SIGABRT, SIGFPE, SIGILL, SIGBUS, SIGTRAP
+  SIGSEGV, SIGABRT, SIGFPE, SIGILL, SIGBUS, SIGTRAP, SIGXCPU
 };
 const int kNumHandledSignals =
     sizeof(kExceptionSignals) / sizeof(kExceptionSignals[0]);
